4a837a29f627be0baea8aaa38ccf4ca9f8732dd8
Mifka Rene <rene.mifka@gmail.com>
2024-04-05 15:56:04 +0200

    Initial commit

---
+++ MelonFearTrainer/FearTrainer.cs
@@ -0,0 +1,391 @@
+﻿using MelonLoader;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MelonFearTrainer
+{
+    public class FearTrainer : MelonMod
+    {
+        private enum ModCategory
+        {
+            Player,
+            Utility
+        }
+
+        private class Setting
+        {
+            public string Name;
+            public bool IsPressed;
+        }
+
+        private Vector3 _currentPosition;
+
+        private readonly Setting[] _playerSettings =
+        {
+            new Setting { Name = "Speed Hack", IsPressed = false },
+            new Setting { Name = "Set Into Sofa", IsPressed = false }
+        };
+
+        private readonly Setting[] _utilitySettings =
+        {
+            new Setting { Name = "Load Day Scene", IsPressed = false },
+            new Setting { Name = "Load Dark Scene", IsPressed = false },
+            new Setting { Name = "Change Food Cook State", IsPressed = false },
+            new Setting { Name = "Toggle Triggers", IsPressed = false }
+        };
+
+        private bool _showMenu;
+
+        private GUIStyle _labelStyle;
+        private GUIStyle _buttonStyle;
+        private GUIStyle _textAreaStyle;
+
+        private readonly Color _buttonBgColor = Color.black;
+        private readonly Color _buttonActiveBgColor = Color.red;
+        private readonly Color _buttonHoverBgColor = Color.gray;
+        private readonly Color _textColor = new Color(0.8f, 0.8f, 0.8f);
+
+        private FirstPersonAIO _playerController;
+
+        public override void OnGUI()
+        {
+            if (_showMenu)
+            {
+                DrawModMenu();
+                Cursor.lockState = CursorLockMode.Confined;
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                ToggleMenu();
+            }
+
+            if (_showMenu)
+            {
+                ApplyChanges();
+            }
+
+            if (_playerController != null)
+            {
+                SupplyPosition(_playerController.transform.position);
+            }
+        }
+
+        public override void OnFixedUpdate()
+        {
+            base.OnFixedUpdate();
+            if (_playerController == null)
+            {
+                var go = GameObject.FindWithTag("Player");
+                if (go != null)
+                {
+                    _playerController = go.GetComponent<FirstPersonAIO>();
+                }
+            }
+        }
+
+        private void InitializeStyles()
+        {
+            _labelStyle = new GUIStyle(GUI.skin.label)
+            {
+                fontSize = 16,
+                fontStyle = FontStyle.Bold,
+                alignment = TextAnchor.MiddleCenter,
+                normal = { textColor = _textColor }
+            };
+
+            _buttonStyle = new GUIStyle(GUI.skin.button)
+            {
+                fontSize = 14,
+                alignment = TextAnchor.MiddleCenter,
+                normal = { background = MakeTex(2, 2, _buttonBgColor) },
+                active = { background = MakeTex(2, 2, _buttonActiveBgColor) },
+                hover = { background = MakeTex(2, 2, _buttonHoverBgColor) },
+            };
+
+            _textAreaStyle = new GUIStyle(GUI.skin.textArea)
+            {
+                fontSize = 14,
+                alignment = TextAnchor.MiddleLeft,
+                normal = { textColor = _textColor },
+                fixedWidth = 50
+            };
+        }
+
+        private string _teleportPosX = "0";
+        private string _teleportPosY = "0";
+        private string _teleportPosZ = "0";
+
+        private void DrawModMenu()
+        {
+            if (_labelStyle == null)
+            {
+                InitializeStyles();
+            }
+
+            Cursor.visible = true; // Show cursor when menu is open
+            float windowWidth = 400f; // Width of the window
+            float windowHeight = 400f; // Height of the window
+
+            Rect windowRect = new Rect(Screen.width - windowWidth - 10f, 10f, windowWidth, windowHeight);
+
+            GUILayout.Window(0, windowRect, id =>
+            {
+                GUILayout.BeginVertical();
+                GUILayout.Label("Mod Menu", _labelStyle);
+
+                DrawSettings(ModCategory.Player);
+                DrawSettings(ModCategory.Utility);
+
+                GUILayout.Label("Description:", _labelStyle);
+
+                GUILayout.Label("Current Position: " + _currentPosition.ToString(), _labelStyle);
+
+                GUILayout.Space(20);
+
+                GUILayout.Label("Teleport Position", _labelStyle);
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("X:", _labelStyle);
+                _teleportPosX = GUILayout.TextField(_teleportPosX, 5, _textAreaStyle);
+                GUILayout.Label("Y:", _labelStyle);
+                _teleportPosY = GUILayout.TextField(_teleportPosY, 5, _textAreaStyle);
+                GUILayout.Label("Z:", _labelStyle);
+                _teleportPosZ = GUILayout.TextField(_teleportPosZ, 5, _textAreaStyle);
+                GUILayout.EndHorizontal();
+
+                GUILayout.BeginHorizontal();
+                if (GUILayout.Button("Teleport", _buttonStyle))
+                {
+                    TeleportToPosition(new Vector3(float.Parse(_teleportPosX), float.Parse(_teleportPosY),
+                        float.Parse(_teleportPosZ)));
+                }
+
+                if (GUILayout.Button("Close", _buttonStyle))
+                {
+                    _showMenu = false;
+                }
+
+                GUILayout.EndHorizontal();
+
+                GUILayout.EndVertical();
+            }, "", GUIStyle.none);
+
+            Cursor.visible = false;
+        }
+
+        private void SupplyPosition(Vector3 position)
+        {
+            _currentPosition = position;
+        }
+
+        private void TeleportToPosition(Vector3 position)
+        {
+            if (_playerController != null)
+            {
+                _playerController.transform.position = position;
+            }
+
+            MelonLogger.Msg($"Teleporting to position: {position}");
+        }
+
+        private void DrawSettings(ModCategory category)
+        {
+            GUILayout.Label(category.ToString(), _labelStyle);
+
+            Setting[] settings = category == ModCategory.Player ? _playerSettings : _utilitySettings;
+            
+            foreach (var setting in settings) 
+            {
+                DrawSetting(setting);
+            }
+        }
+
+        private void DrawSetting(Setting setting)
+        {
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button(setting.Name, _buttonStyle))
+            {
+                MelonLogger.Msg("Toggled " + setting.Name + " to " + !setting.IsPressed);
+
+                setting.IsPressed = !setting.IsPressed;
+            }
+
+            GUILayout.EndHorizontal();
+        }
+
+        private bool _triggersDrawn = false;
+        private bool _speedChanged = false;
+
+        private void ApplyChanges()
+        {
+            if (_playerSettings[0].IsPressed && !_speedChanged)
+            {
+                MelonLogger.Msg("Applying Speed hack");
+                Time.timeScale = 2f;
+                _speedChanged = true;
+            }
+            else if (_speedChanged && _playerSettings[0].IsPressed)
+            {
+                Time.timeScale = 1f;
+                _speedChanged = false;
+            }
+
+            if (_playerSettings[1].IsPressed)
+            {
+                MelonLogger.Msg("Set Into Sofa enabled");
+
+                GameObject sofa = GameObject.Find("Sofa_House");
+                if (sofa != null)
+                {
+                    sofa.GetComponent<sofaSwitch>().StartCoroutine("SitOnSofa");
+                }
+
+                _playerSettings[1].IsPressed = false;
+            }
+
+            if (_utilitySettings[0].IsPressed)
+            {
+                MelonLogger.Msg("Load Day Scene enabled");
+                SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+                SceneManager.LoadScene("Scene 1");
+
+                _utilitySettings[0].IsPressed = false;
+            }
+
+            if (_utilitySettings[1].IsPressed)
+            {
+                MelonLogger.Msg("Load Dark Scene enabled");
+
+                SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+                SceneManager.LoadScene("Scene 1 Dark");
+
+                _utilitySettings[1].IsPressed = false;
+            }
+
+            if (_utilitySettings[2].IsPressed)
+            {
+                MelonLogger.Msg("Changed Food Cook State");
+
+                GameObject food = GameObject.Find("food");
+                if (food != null)
+                {
+                    var foodComp = food.GetComponent<food>();
+                    foodComp.foodCooked = !foodComp.foodCooked;
+                }
+
+                _utilitySettings[2].IsPressed = false;
+            }
+
+            if (_utilitySettings[3].IsPressed)
+            {
+                if (!_triggersDrawn)
+                {
+                    var go = GameObject.Find("TRIGGERS");
+                    if (go == null)
+                    {
+                        return;
+                    }
+
+                    _triggersDrawn = true;
+
+                    var colliders = go.GetComponentsInChildren<BoxCollider>();
+                    if (colliders == null)
+                    {
+                        return;
+                    }
+
+                    foreach (var collider in colliders)
+                    {
+                        if (collider == null || collider.gameObject == null)
+                        {
+                            continue;
+                        }
+
+                        LineRenderer lineRenderer = collider.gameObject.AddComponent<LineRenderer>();
+                        if (lineRenderer == null)
+                        {
+                            continue;
+                        }
+
+                        var shader = Shader.Find("Sprites/Default");
+                        if (shader == null)
+                        {
+                            continue;
+                        }
+
+                        lineRenderer.material = new Material(shader);
+                        lineRenderer.startWidth = 0.01f;
+                        lineRenderer.endWidth = 0.01f;
+                        var randColor = Random.ColorHSV();
+                        lineRenderer.startColor = randColor;
+                        lineRenderer.endColor = randColor;
+
+                        var cubeVertices = new[]
+                        {
+                            new Vector3(1, 1, 1), new Vector3(1, 1, -1), new Vector3(-1, 1, -1),
+                            new Vector3(-1, 1, 1), new Vector3(1, 1, 1), new Vector3(1, -1, 1),
+                            new Vector3(1, -1, -1), new Vector3(-1, 1, -1), new Vector3(-1, -1, 1),
+                            new Vector3(1, -1, 1), new Vector3(1, 1, -1), new Vector3(1, -1, -1),
+                            new Vector3(-1, 1, -1), new Vector3(-1, -1, -1), new Vector3(-1, 1, 1),
+                            new Vector3(-1, -1, 1), new Vector3(1, 1, 1), new Vector3(-1, 1, -1),
+                            new Vector3(-1, -1, -1), new Vector3(1, -1, -1)
+                        };
+                        lineRenderer.positionCount = 20;
+                        lineRenderer.useWorldSpace = false;
+                        lineRenderer.SetPositions(cubeVertices);
+                    }
+                }
+            }
+            else
+            {
+                if (_triggersDrawn)
+                {
+                    MelonLogger.Msg("Triggers are disabled");
+                    var go = GameObject.Find("TRIGGERS");
+                    _triggersDrawn = false;
+                    if (go != null)
+                    {
+                        var colliders = go.GetComponentsInChildren<BoxCollider>();
+                        foreach (var collider in colliders)
+                        {
+                            var lineRenderer = collider.gameObject.GetComponent<LineRenderer>();
+                            if (lineRenderer != null)
+                            {
+                                Object.DestroyImmediate(lineRenderer);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ToggleMenu()
+        {
+            _showMenu = !_showMenu;
+        }
+
+        private Texture2D MakeTex(int width, int height, Color col)
+        {
+            Color[] pix = new Color[width * height];
+            for (int i = 0; i < pix.Length; ++i)
+            {
+                pix[i] = col;
+            }
+
+            Texture2D result = new Texture2D(width, height);
+            result.SetPixels(pix);
+            result.Apply();
+            return result;
+        }
+
+        public override void OnInitializeMelon()
+        {
+            base.OnInitializeMelon();
+
+            MelonLogger.Msg("FearTrainer loaded!");
+        }
+    }
+}
---
+++ MelonFearTrainer/copy.cmd
@@ -0,0 +1,15 @@
+@echo off
+
+cls
+
+echo Building MelonFearTrainer.dll
+
+copy "MelonFearTrainer.dll" "A:\SteamLibrary\steamapps\common\Fears to Fathom - Episode 1\Mods"
+
+echo Succesfully copied TasTools.dll to Fears to Fathom - Episode 1\Mods
+
+echo Starting Fears to Fathom - Episode 1...
+
+start steam://rungameid/1671340
+
+exit 0
